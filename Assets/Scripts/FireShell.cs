02482c5542bc0d62d1c58174c9103ed105312d37
DevVHO <161318358+DevVHO@users.noreply.github.com>
2024-10-24 14:48:14 +0100

    Calculating Trajectories Part 2
    Nesta parte estive modificando o Script FireShell, assim conseguindo rotacionar da forma correta. Além de organizar corretamente a workspace e colocar os gameObjects e outros em seu devido lugar

--- Assets/Scripts/FireShell.cs
+++ Assets/Scripts/FireShell.cs
@@ -7,15 +7,46 @@ public class FireShell : MonoBehaviour
     public GameObject bullet;
     public GameObject turret;
     public GameObject enemy;
-
-    private float rotSpeed = 2;
+    public Transform turretBase;
+    float speed = 15;
+    float rotSpeed = 2;
     void CreateBullet()
     {
         GameObject shell = Instantiate(bullet, turret.transform.position, turret.transform.rotation);
     }
-    void Start()
+
+    void RotateTurret()
     {
-        
+        float? angle = CalculateAngle(true);
+        if(angle != null)
+        {
+            turretBase.localEulerAngles = new Vector3(360f - (float)angle, 0f, 0f);
+        }
+    }
+
+    float? CalculateAngle(bool low)
+    {
+        Vector3 targetDir = enemy.transform.position - this.transform.position;
+        float y = targetDir.y;
+        targetDir.y = 0f;
+        float x = targetDir.magnitude;
+        float gravity = 9.8f;
+        float sSqr = speed * speed;
+        float underThesSqrRoot = (sSqr * sSqr) - gravity * (gravity * x * x + 2 * y * sSqr);
+
+        if (underThesSqrRoot >= 0f)
+        {
+            float root = Mathf.Sqrt(underThesSqrRoot);
+            float highAngle = sSqr + root;
+            float lowAngle = sSqr - root;
+
+            if (low)
+                return (Mathf.Atan2(lowAngle, gravity * x) * Mathf.Rad2Deg);
+            else
+                return (Mathf.Atan2(highAngle, gravity * x) * Mathf.Rad2Deg);
+        }
+        else
+            return null;
     }
 
     // Update is called once per frame
@@ -24,6 +55,7 @@ public class FireShell : MonoBehaviour
         Vector3 direction = (enemy.transform.position - this.transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0.0f, direction.z));
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookRotation, Time.deltaTime * rotSpeed);
+        RotateTurret();
         if (Input.GetKeyDown(KeyCode.Space))
         {
             CreateBullet();
